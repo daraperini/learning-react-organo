8e40f943f02c285bed6d921b997f8b551b21f5da
Dara Lahm Perini <daraperini@gmail.com>
2023-11-29 16:05:30 -0300

    Versao typescript

---
+++ organo-versao-typescript/src/componentes/Colaborador/index.tsx
@@ -0,0 +1,24 @@
+import { IColaborador } from "../../compartilhado/interfaces/IColaborador";
+import "./Colaborador.css";
+
+interface ColaboradorProps {
+  colaborador: IColaborador;
+  corDeFundo: string;
+}
+
+const Colaborador = ({ colaborador, corDeFundo }: ColaboradorProps) => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+        <h5>{new Date(colaborador.data).toLocaleDateString()}</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
---
+++ organo-versao-typescript/src/componentes/Time/index.tsx
@@ -0,0 +1,33 @@
+import { IColaborador } from "../../compartilhado/interfaces/IColaborador";
+import Colaborador from "../Colaborador";
+import "./Time.css";
+
+interface TimeProps {
+  corPrimaria: string
+  corSecundaria: string
+  nome: string
+  colaboradores: IColaborador[] 
+}
+
+const Time = ({ colaboradores, corPrimaria, corSecundaria, nome }: TimeProps) => {
+  const css = { backgroundColor: corSecundaria };
+
+  return colaboradores.length > 0 ? (
+    <section className="time" style={css}>
+      <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
+      <div className="colaboradores">
+        {colaboradores.map((colaborador) => (
+          <Colaborador
+            corDeFundo={corPrimaria}
+            key={colaborador.nome}
+            colaborador={colaborador}
+          />
+        ))}
+      </div>
+    </section>
+  ) : (
+    <></>
+  );
+};
+
+export default Time;
